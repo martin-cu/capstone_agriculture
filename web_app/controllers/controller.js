77a08fb0782ed4c5a83ac8ecd6ca402d933f3534
y2saur <y2_aquino@dlsu.edu.ph>
2021-09-29 20:47:09 +0800

    tested add pesticide and seed

--- web_app/controllers/controller.js
+++ web_app/controllers/controller.js
@@ -1,13 +1,29 @@
 const materialModel = require('../models/materialModel');
 
-exports.addMaterial = function(req,res){
+exports.addMaterials = function(req,res){
     //Check if item already exists
 
     //if exists, update
 
     //else add new
+    
+    res.render("test",{test_data : "SELECT"});
+}
+
+exports.addPesticide = function(req,res){
+    console.log("Added one pesticide");
     materialModel.addPesticide(function(err,result){
-        res.render("test",{test_data : "ADDED"});
     });
     
+    html_data = {msg : "Done. Pesticide added."}
+    res.send(html_data);
+}
+
+exports.addSeed = function(req,res){
+    seed = "Dinorado2"
+    materialModel.addSeed(seed, function(err,result){
+    });
+
+    html_data = {msg : "Done. Seed added."}
+    res.send(html_data);
 }
--- web_app/models/materialModel.js
+++ web_app/models/materialModel.js
@@ -6,3 +6,11 @@ exports.addPesticide = function(query, next) {
     var sql = "INSERT INTO capstone_agriculture_db.pesticide_table (pesticide_name, pesticide_desc, current_amount) VALUES ('Glorious','Used for dinorado', 600);"
 	mysql.query(sql, next);
 }
+
+exports.addSeed = function(seed_name, query, next) {
+
+    var sql = "INSERT INTO capstone_agriculture_db.seed_table (seed_name, seed_desc, current_amount, maturity_days, average_yield) VALUES (?,'Used for dinorado', 500, 140, 100);"
+	sql = mysql.format(sql, seed_name);
+    mysql.query(sql, next);
+}
+
