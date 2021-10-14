5650e6fe8e50da02a1d688c953389852a712beb7
Martin Cu <martin_cu@dlsu.edu.ph>
2021-10-14 13:04:39 +0800

    merge

--- pestdisease.sql
+++ pestdisease.sql
@@ -11,11 +11,27 @@ WHERE d.disease_id = 1;
 SELECT p.pest_id, p.pest_name, wt.weather as factor, wt.weather_desc as description, "weather" as type FROM pest_table p
 INNER JOIN weather_pest wp ON p.pest_id = wp.pest_id
 INNER JOIN weather_table wt ON wt.weather_id = wp.weather_id
+WHERE p.pest_id = 1
 UNION
 SELECT p.pest_id, p.pest_name, s.season_name as factor, s.season_desc as description, "season" as type FROM pest_table p
 INNER JOIN season_pest sp ON p.pest_id = sp.pest_id
 INNER JOIN seasons s ON s.season_id = sp.season_pest
+WHERE p.pest_id = 1
 UNION
-SELECT p.pest_id, p.pest_name, s.stage_name as factor, s.stage_desc as description, "stage" as type  FROM pest_table p
+SELECT p.pest_id, p.pest_name, s.stage_name as factor, s.stage_desc as description, "stage" as type FROM pest_table p
 INNER JOIN stages_pest sp ON sp.pest_id = p.pest_id
-INNER JOIN stages s ON s.stage_id = sp.stages_pest_id;
+INNER JOIN stages s ON s.stage_id = sp.stages_pest_id
+WHERE p.pest_id = 1
+UNION
+SELECT p.pest_id, p.pest_name, ft.farm_type as factor, ft.farm_type_desc as description, "farm type" as type FROM pest_table p
+INNER JOIN farmtypes_pest ftp ON p.pest_id = ftp.pest_id
+INNER JOIN farm_types ft ON ft.farm_type_id = ftp.farm_type_id
+WHERE p.pest_id = 1
+UNION
+SELECT p.pest_id, p.pest_name, ft.fertilizer_name as factor, ft.fertilizer_desc as description, "fertilizer" as type FROM pest_table p
+INNER JOIN fertilizer_pest fp ON p.pest_id = fp.pest_id
+INNER JOIN fertilizer_table ft ON ft.fertilizer_id = ft.fertilizer_id
+WHERE p.pest_id = 1;
+
+
+
--- web_app/controllers/environmentController.js
+++ web_app/controllers/environmentController.js
@@ -6,14 +6,104 @@ var request = require('request');
 
 exports.getPestDiseaseManagement = function(req, res) {
 	var html_data = {};
-	html_data = js.init_session(html_data, 'role', 'name', 'username', 'pest_and_disease');
-	res.render('/pest_diseases', html_data);
+	pestdiseaseModel.getAllPests(function(err,pests){
+		if(err){
+			throw err;
+		}
+		else{
+			pestdiseaseModel.getAllDiseases(function(err,diseases){
+				if(err){
+					throw err;
+				}
+				else{
+					html_data["pests"] = pests;
+					html_data["diseases"] = diseases;
+					html_data = js.init_session(html_data, 'role', 'name', 'username', 'pest_and_disease');
+					res.render('pest_disease', html_data);
+				}
+			});
+		}
+	});
 }
+
+exports.getPestFactors = function(req,res){
+	var html_data = {};
+
+	var type = req.params.type;
+	var id = req.params.name;
+
+	if(type == "Pest"){
+		pestdiseaseModel.getPestFactors(id, function(err, factors){
+			if(err){
+				throw err;
+			}
+			else{
+				pestdiseaseModel.getPestDetails({pest_id : id}, function(err, details){
+					if(err){
+						throw err;
+					}
+					else{
+						if(details.length == 0){
+	
+						}
+						else{
+							console.log(details[0].pest_name);
+							console.log(factors);
+							html_data['pest_name'] =  details[0].pest_name;
+							html_data["factors"] = factors;
+							js.init_session(html_data, 'role', 'name', 'username', 'pest_and_disease');
+							res.render('pest_disease2', html_data);
+						}
+					}
+				});
+			}
+		});
+	}
+	else{
+		pestdiseaseModel.getDiseaseFactors(id, function(err, factors){
+			if(err){
+				throw err;
+			}
+			else{
+				pestdiseaseModel.getDiseaseDetails({disease_id : id}, function(err, details){
+					if(err){
+						throw err;
+					}
+					else{
+						if(details.length == 0){
+	
+						}
+						else{
+							console.log(details[0].disease_name);
+							console.log(factors);
+							html_data['pest_name'] =  details[0].disease_name;
+							html_data["factors"] = factors;
+							js.init_session(html_data, 'role', 'name', 'username', 'pest_and_disease');
+							res.render('pest_disease2', html_data);
+						}
+					}
+				});
+			}
+		});
+	}
+	
+}
+
+
 
 exports.getNurientManagement = function(req, res) {
 	var html_data = {};
 	html_data = js.init_session(html_data, 'role', 'name', 'username', 'nutrient_mgt');
 	res.render('/nutrient_mgt', html_data);
+}
+
+
+
+
+exports.addPest = function(req,res){
+	var pest_data = req.query.pest_data;
+	pestdiseaseModel.addPest(pest_data, function(err, result){
+	});
 }
 
 
