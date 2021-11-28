86c6b52310854fae21cda4e25e0ba1cfda10b245
yenwich <61381788+yenwich@users.noreply.github.com>
2021-11-28 23:35:00 +0800

    Final UI

--- web_app/views/UPDATED/assets/js/bs-init.js
+++ web_app/views/UPDATED/assets/js/bs-init.js
@@ -4,4 +4,10 @@ if (window.innerWidth < 768) {
 
 $(document).ready(function(){
 	AOS.init();
+	$('[data-bss-tooltip]').tooltip();
+
+	$('[data-bss-chart]').each(function(index, elem) {
+		this.chart = new Chart($(elem), $(elem).data('bss-chart'));
+	});
+
 });
