7014d37ce8b13c1978f8f9647705072fa9846501
y2saur <y2_aquino@dlsu.edu.ph>
2021-12-16 11:48:24 +0800

    Minor upgrades

--- web_app/public/js/materials.js
+++ web_app/public/js/materials.js
@@ -279,7 +279,11 @@ $(document).on("change",'.purchase_item_type', function(){
     var type = $(this).val();
     var select = "item" + id.charAt(id.length - 1);
     // alert(select);
-
+    if(type == "Seed")
+        $("#item_unit" + id.charAt(id.length - 1)).text("grams");
+    else{
+        $("#item_unit" + id.charAt(id.length - 1)).text("Bags");
+    }
     $(".material_" + select).remove();
     $.get("/ajaxGetMaterials", {type : type}, function(result){
         var i;
