51a54a7ef2e2cacd7108899cdd4569bc05c7458f
y2saur <y2_aquino@dlsu.edu.ph>
2022-04-25 12:55:23 +0800

    Update sms-mt.js

--- controllers/sms-mt.js
+++ controllers/sms-mt.js
@@ -2,11 +2,20 @@ const js = require('../public/js/session.js');
 
 var request = require("request");
 
-var shortcode = '5119';
-var access_token = 'A-2szoYus7mB13l5axDrr_1234AApSz8eu236GRNsoBQ';
+
+var app_id = 'X4kxHEG59nuXkT8ynri5KGuyR4xzHLbr';
+var app_secret = '280f39f528dc56db22e5f31a4a87dad0969d5cce23f659e3e3e888a2371fe585';
+var code = 'osg68ErhoXxaACygn5yS7yAqRfB6E49S7qE75Id6z4puxBayXhpGMdzFzxeb6FKded8uayLbE8kxroI4zEB8taBrxeseAbLdsebpBLf4Rgp9Udkz6gFrnLdjC7oe9xu6begLFEyMj7FnRapRhjazkAuxaEyEIAoEdLSL6ALMfaqnE5SqjxXnCdX8johkpeBRs';
+
+
+
+
+
+var shortcode = '21585119';
+var access_token = 'A-2szoYus7mB13l5axDrr_1234AApSz8eu236GRNsoBQ'; //not yet final
 var address = '9173028128';
 var clientCorrelator = '264801';
-var message = 'NodeJS SMS Test';
+var message = 'THIS IS A REPLY TO YOU';
 
 var options = { method: 'POST',
   url: 'https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/' + shortcode + '/requests',
@@ -23,6 +32,48 @@ var options = { method: 'POST',
 
 
 
+exports.globe_inbound_msg = function(req, res){
+  // console.log(req);
+  
+  console.log("--------------------");
+  
+  console.log(req.body);
+  console.log("~~~~~~~~~~~~PROCESS DATA~~~~~~~~~~~~");
+  console.log(req.body.inboundSMSMessageList.inboundSMSMessage);
+  console.log("~~~~~~~~~~~~STORE DATA~~~~~~~~~~~~");
+  
+
+  // this.globe_outbound_msg;
+  this.getAccessToken;
+  return;
+  }
+   
+exports.globe_outbound_msg = function(req, res){
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
+  });
+  
+}
+
+exports.getAccessToken = function(req,res){
+  var options = { method: 'POST',
+  url: 'https://developer.globelabs.com.ph/oauth/access_token',
+  qs: 
+   { 'app_id': app_id,
+     'app_secret': app_secret,
+     'code': code },
+  headers: 
+   { 'cache-control': 'no-cache' } };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
+}
+
+
 
 
 exports.test_globe = function(req,res){
@@ -36,27 +87,12 @@ exports.test_globe = function(req,res){
 
 
 
-
-
 exports.test_globe2 = function(req,res){
   console.log(req);
   // request("https://developer.globelabs.com.ph/oauth/access_token?app_id=X4kxHEG59nuXkT8ynri5KGuyR4xzHLbr&app_secret=280f39f528dc56db22e5f31a4a87dad0969d5cce23f659e3e3e888a2371fe585&code=21585119", function(err, response){
   //   console.log(response);
   // });
 }
-
-
-exports.globe_inbound_msg = function(req, res){
-  console.log(req);
-
-  console.log("--------------------");
-
-  console.log(req.body);
-
-  console.log("~~~~~~~~~~~~PROCESS DATA~~~~~~~~~~~~");
-  console.log("~~~~~~~~~~~~STORE DATA~~~~~~~~~~~~");
-}
-
 
 
 
